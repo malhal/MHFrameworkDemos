dc7686d9578123aa1dbdc2567f765f12f75f0469
Malcolm Hall <malhal@users.noreply.github.com>
2018-09-11 23:19:12 +0100

    Updates

--- MUIDemo/AppDelegate.m
+++ MUIDemo/AppDelegate.m
@@ -15,7 +15,6 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
